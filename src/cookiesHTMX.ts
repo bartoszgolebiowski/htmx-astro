30ec3357058408748bcd47fb1901cddac589125c
golebiowskib <bartosz.golebiowski@@ttpsc.pl>
2023-05-21 22:41:30 +0200

    init commit

---
+++ src/cookiesHTMX.ts
@@ -0,0 +1,64 @@
+import type { AstroCookies } from "astro";
+
+const STEP_COOKIE_NAME = "multistep-form-htmx";
+
+type MultistepForm = {
+    currentStep: number
+    step1?: {
+        name: string;
+        email: string;
+    },
+    step2?: {
+        address: string;
+        city: string;
+    },
+    step3?: {
+        cardNumber: string;
+        cardExpiry: string;
+    },
+}
+
+type Steps = "step1" | "step2" | "step3"
+
+const getMultistepForm = (cookies: AstroCookies) => {
+    const cookiesRaw = cookies.get(STEP_COOKIE_NAME);
+    if (!cookiesRaw.value) {
+        return {
+            currentStep: 1
+        }
+    }
+    const multistep = cookiesRaw.json() as MultistepForm;
+    return multistep;
+}
+
+export const getCurrentStep = (cookies: AstroCookies) => {
+    const multistep = getMultistepForm(cookies)
+    return multistep.currentStep
+}
+
+export const getDefaultValues = <T extends Steps>(cookies: AstroCookies, step: T) => {
+    const multistep = getMultistepForm(cookies)
+    return multistep[step]
+}
+
+export const submitValues = <T extends Steps>(cookies: AstroCookies,
+    step: T,
+    values: MultistepForm[T]
+) => {
+    const multistep = getMultistepForm(cookies)
+    if (step === "step1") {
+        multistep.currentStep = 2;
+    }
+    if (step === "step2") {
+        multistep.currentStep = 3;
+    }
+    if (step === "step3") {
+        multistep.currentStep = 4;
+    }
+    multistep[step] = values;
+    cookies.set(STEP_COOKIE_NAME, JSON.stringify(multistep), { httpOnly: true, sameSite: "lax", secure: true });
+}
+
+export const clearCookies = (cookies: AstroCookies) => {
+    cookies.set(STEP_COOKIE_NAME, {}, { httpOnly: true, sameSite: "lax", secure: true });
+}
